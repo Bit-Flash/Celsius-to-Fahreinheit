d799811eb3234045496601c6235fc31431d26909
BitFlash <81009375+Bit-Flash@users.noreply.github.com>
2022-08-04 21:26:29 +0200

    Final

--- simplecalc.js
+++ simplecalc.js
@@ -1,8 +1,4 @@
 // Crad zu Celsius Umrechnen
-const eele = document.getElementById("btn");
-            eele.addEventListener("click", ctof);
-
-
             function ctof() {
 
             document.getElementById('output').innerHTML = "Fahrenheit: " + (document.getElementById("celsi").value * (9 / 5) + 32);
